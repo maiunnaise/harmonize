7a841e9312503e72a6b181880f20cb45a9142aac
maiunnaise <maiouwenneuh@gmail.com>
2023-12-14 11:10:18 +0100

    homeStudent

--- src/pages/homeStudent.js
+++ src/pages/homeStudent.js
@@ -1,13 +1,19 @@
 import './homeStudent.css';
 import PurpleDiv from '../components/PurpleDiv';
 import GreyDiv from '../components/GreyDiv';
+import { Link } from 'react-router-dom';
+import HistoryButton from '../components/HistoryButton';
 
 function Course({course}){
     return (
         <div className='courseContent'>
-            <h2 className='CourseName'>{course.name} <span style={{color:"#B0B0B0"}}>- {course.difficulty}</span></h2>
+            <h2 className='CourseName coursePadding'>{course.name} <span style={{color:"#B0B0B0"}}>- {course.difficulty}</span></h2>
             <PurpleDiv content={<Seance seance= {course.nextClass}/>}/>
-            {course.teacher.gender =="male" ? <p className='CourseTeach'>M.   {course.teacher.name}</p> : <p className='CourseTeach'>Mme. {course.teacher.name}</p>}
+            {course.teacher.gender =="male" ? <p className='CourseTeach coursePadding'>M.   {course.teacher.name}</p> : <p className='CourseTeach coursePadding'>Mme. {course.teacher.name}</p>}
+            <h3 className='CourseActivities coursePadding'>Activités</h3>
+            {course.exercices.map((exercice) => {
+                return <Actvity activity={exercice}/>;
+            })}
         </div>
     );
 }
@@ -22,6 +28,21 @@ function Seance({seance}){
     );
 }
 
+function Actvity({activity}){
+    return(
+        <div className='courseActivity'>
+            <div className={activity.status =="toDo" ? "redAct": "greenAct"}></div>
+            <div className='courseActivityLine'>
+                <p>{activity.name}</p>
+                <p>pour le {activity.dueAt}</p>
+            </div>
+            <Link to='/activity' className='courseActivityButton'>
+                <button>Voir</button>
+            </Link>
+        </div>
+    )
+}
+
 const courses = [
     {name:"Guitare", difficulty:"Débutant", 
         teacher:{
@@ -30,14 +51,14 @@ const courses = [
         nextClass:{
             date:"13-12-2023", startedAt :"9h30", endedAt:"11h30", description :"Travail sur le solfège lalalala"
         },
-        exercices:{
-            0:{
+        exercices:[
+            {
                 name:"Jouer partition 1", dueAt:"14/12/2023", status:"toDo"
             },
-            1:{
+            {
                 name:"Jouer partition 2", dueAt:"14/12/2023", status:"done"
             }
-        }
+        ]
     },
     {name:"Basse", difficulty:"Débutant", 
         teacher:{
@@ -46,23 +67,25 @@ const courses = [
         nextClass:{
             date:"13-12-2023", startedAt :"9h30", endedAt:"11h30", description :"Travail sur le solfège lalalala"
         },
-        exercices:{
-            0:{
-                name:"Jouer partition 1", dueAt:"14/12/2023", status:"toDo"
+        exercices:[
+            {
+                name:"Jouer partition 3", dueAt:"14/12/2023", status:"toDo"
             },
-            1:{
-                name:"Jouer partition 2", dueAt:"14/12/2023", status:"done"
+            {
+                name:"Jouer partition 4", dueAt:"14/12/2023", status:"done"
             }
-        }
+        ]
     }
 ];
 
 export default function HomeStudent(){
     return (
         <div className="homeStudent content">
-            {courses.map((course, index) => {
-                return <GreyDiv content={<Course key={index} course={course} />}/>;
+            {courses.map((course) => {
+                return <GreyDiv content={<Course course={course} />}/>;
             })}
+
+            <HistoryButton buttonText={"Historique d'activités"}/>
         </div>
     );
 }
