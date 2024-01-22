aa834db3e17e9682222276778f0f166bde4ac3f1
Paul Lebée <paul.lebee@epita.fr>
2024-01-22 10:19:38 +0100

    link API CoursPage

--- src/pages/CoursPage.js
+++ src/pages/CoursPage.js
@@ -4,35 +4,45 @@ import GreyDiv from '../components/GreyDiv.js';
 import SimpleHeader from '../components/simpleHeader.js';
 import { useSearchParams, useParams } from 'react-router-dom'
 import CheckLogin from '../components/checkLogin.js';
+import { getAPI, postAPI, deleteAPI, putAPI} from '../components/fetchAPI.js';
 
 function Cours(){
     CheckLogin();
+
     const [queryParameters] = useSearchParams()
     const {coursId} = useParams();
-    //Donne le cours dont l'id est coursId
-    let cours = coursData.filter(e => e.id === Number(coursId))[0];
+    const [cours, setCours] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            await getAPI('cours-app/'+coursId, setCours);
+        };
+        fetchData();
+    }, []);
+    
     return (
+        cours.length != 0 ? 
         <div className="simpleContent">
             <SimpleHeader />
             <div id="CoursPage">
-                <h2>{cours.titre} <span className="greyText"> · {cours.category} </span></h2>
-                <p className='boldText'>{cours.desc}</p>
+                <h2>{cours.Title} <span className="greyText"> · {cours.Instrument.Name} {cours.Difficulty}</span></h2>
+                <p className='boldText'>{cours.description}</p>
                 <p>{cours.content}</p>
             </div>
         </div>
+        : null
     )
 }
 
-let coursData = [{id: 5, titre: "Les notes", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-category: "Solfège", img: "/img/cours-image.jpg",
-content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-{id: 3, titre: "Les accords", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-category: "Solfège", img: "/img/cours-image.jpg",
-content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-{id: 2, titre: "Les notes", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-category: "Piano", img: "/img/cours-image.jpg",
-content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}]
+// let coursData = [{id: 5, titre: "Les notes", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+// category: "Solfège", img: "/img/cours-image.jpg",
+// content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+// {id: 3, titre: "Les accords", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+// category: "Solfège", img: "/img/cours-image.jpg",
+// content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+// {id: 2, titre: "Les notes", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+// category: "Piano", img: "/img/cours-image.jpg",
+// content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}]
 
 export default function CoursPage(){
     return (
