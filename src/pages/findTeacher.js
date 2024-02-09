8fdb979b3492cfb30d8db6115969ea31e9e9a9d1
Paul Lebée <paul.lebee@epita.fr>
2024-02-09 16:45:59 +0100

    findTeacher Geoloc + filtres

--- src/pages/findTeacher.js
+++ src/pages/findTeacher.js
@@ -15,15 +15,17 @@ function TeachersDesc({teacher, onSendRequest}){
     }, []);
     return (
         <div id={teacher.User.id}>
-            <img className="findTeacherImg"src="../logo192.png" alt='${teacher.prenom} ${teacher.nom}'/>
             <div>
-                <div id="teacherName">
-                    <h2>{teacher.User.prenom} {teacher.User.nom} <span className="greyText"> · {teacher.city} </span></h2>
-                </div>
-                <div className='instrumentsTeach'>
-                    {teacher.User.userInstruments.map((instrument, index) => {
-                        return <p key={index} className="instrumentText">{instrument.Instrument.Name}</p>;
-                    })}
+                <img className="findTeacherImg"src="../logo192.png" alt='${teacher.prenom} ${teacher.nom}'/>
+                <div>
+                    <div id="teacherName">
+                        <h2>{teacher.User.prenom} {teacher.User.nom} <span className="greyText"> · {teacher.city} </span></h2>
+                    </div>
+                    <div className='instrumentsTeach'>
+                        {teacher.User.userInstruments.map((instrument, index) => {
+                            return <p key={index} className="instrumentText">{instrument.Instrument.Name}</p>;
+                        })}
+                    </div>
                 </div>
             </div>
             <p className="textDesc">{teacher.User.description}</p>
@@ -36,54 +38,162 @@ function TeachersDesc({teacher, onSendRequest}){
 }
 
 
+
 function FindTeachersDiv(){
     const [teachers, setTeachers] = useState([]);
+    const [instruments, setInstruments] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
     const [selectedTeacher, setSelectedTeacher] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            await getAPI("teachers", setTeachers);
+            await getAPI("instruments", setInstruments);
         };
         fetchData();
 
     }, []);
 
-    useEffect(() => {
-        if (teachers.length > 0){
-            setFilteredData(teachers);
-        }
-    }, [teachers])
+    // useEffect(() => {
+    //     if (teachers.length > 0){
+    //         setFilteredData(teachers);
+    //     }
+    // }, [teachers])
 
     
-    const handleSearch = (searchTerm) => {
-        if (searchTerm === '') {
-            setFilteredData(teachers);
-            return;
-        }
+    // const handleSearch = (searchTerm) => {
+    //     if (searchTerm === '') {
+    //         setFilteredData(teachers);
+    //         return;
+    //     }
+    //     const filteredResults = teachers.filter((item) => {
+    //         if (item.city !== null) {
+    //             return item.city.toLowerCase().includes(searchTerm.toLowerCase());
+    //         }
+    //         return false; 
+    //     });
     
-        const filteredResults = teachers.filter((item) => {
-            if (item.city !== null) {
-                return item.city.toLowerCase().includes(searchTerm.toLowerCase());
+    //     setFilteredData(filteredResults);
+    // };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target.difficulty.value);
+        console.log(e.target.instrument.value);
+        console.log(e.target.ville.value);
+        console.log(e.target.rythm.value);
+
+        // const fetchData = async () => {
+        //     await getAPI("instruments", setInstruments);
+        // };
+        // fetchData();
+    
+    }
+
+    //Geolocalisation
+    function getPosition() {
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        }
+
+        function showPosition(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(latitude, longitude);
+            reverseGeocode(latitude, longitude);
+        }
+        
+        function reverseGeocode(latitude, longitude) {
+            const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const townName = findTownName(data);
+                    console.log(townName);
+                    let searchInput = document.querySelector('.SearchBar');
+                    searchInput.value = townName;
+                    DisplaySelect();
+                    // handleSearch(townName);
+                })
+                .catch(error => {
+                    console.error('Erreur:', error);
+                });
+        }
+        
+        function findTownName(data) {
+            if(data.address.municipality != undefined){
+                return data.address.municipality;
+            } else if (data.address.suburb != undefined){
+                return data.address.suburb;
+            } 
+            else {
+                return 'Ville non trouvée';
             }
-            return false; 
-        });
-    
-        setFilteredData(filteredResults);
-    };
+        }
+    }
 
+    //affiche les filtres un par un 
+    function DisplaySelect(){
+        let instruments = document.getElementById('instrumentSelect');
+        let town = document.querySelector('.searchBtns');
+        let rythm = document.getElementById('rythmSelect');
+        let input = document.querySelector('.divSearchTeacher > form > input');
+
+        if(instruments.style.display !== "block"){
+            instruments.style.display = "block";
+        }
+        else if(town.style.display !== "flex"){
+            town.style.display = "flex";
+        }
+        else if(rythm.style.display !== "block"){
+            rythm.style.display = "block";
+        }
+        else if(input.style.display !== "block"){
+            input.style.display = "block";
+        }
+
+    }
 
     return (
         <div className='content searchTeacher'>
             <GreyDiv content={
                 <div className="divSearchTeacher">
                     <h2>Trouver un enseignant</h2>
-                    <div className="searchBtns"> 
-                        <Link to="/nextVersion"> 
-                            <button>Autour de moi</button>
-                        </Link>
-                        <SearchBar onSearch={handleSearch}/>
-                    </div>
+                    <form onSubmit={handleSubmit}>
+                        Je recherche un cours
+                        <select id="difficulty" name="difficulty" onChange={DisplaySelect}>
+                           <option value="">...</option> 
+                           <option value="Débutant">Débutant</option>
+                            <option value="Intermédiaire">Intermédiaire</option>
+                            <option value="Avancé">Avancé</option>
+                        </select>
+                        <br/>
+                        <div id="instrumentSelect">
+                            de
+                            <select id="instrument" name="instrument" onChange={DisplaySelect}>
+                                <option value="">...</option>
+                                {instruments.map((instrument, index) => {
+                                    return <option key={index} value={instrument.id}>{instrument.Name}</option>;
+                                })}
+                            </select>
+                        </div>
+                        <div className="searchBtns">
+                            à  
+                            <button type="button" onClick={getPosition}>
+                                <img src="/logo/icons/geoloc.png" alt="geoloc logo"/>
+                            </button>
+                            {/* <SearchBar onSearch={DisplaySelect}/> */}
+                            <input type="text" name="ville" className="SearchBar" placeholder="Rechercher" onChange={DisplaySelect}/>
+                        </div>
+                        <div id="rythmSelect">
+                            à un rythme de 
+                            <select id="rythm" name="rythm" onChange={DisplaySelect}>
+                                <option value="">...</option>
+                                <option value="1">1 fois par semaine</option>
+                                <option value="2">2 fois par semaine</option>
+                            </select>
+                        </div>
+                        <input type="submit" value="Voir les cours" />
+                    </form>
                 </div>
             }/>
             {filteredData.map((teacher, index) => {
@@ -94,7 +204,7 @@ function FindTeachersDiv(){
             {selectedTeacher.length !=0 && (
                 <SendCourseRequest teacher={selectedTeacher} onClose={() => setSelectedTeacher([])} />
             )}
-            
+            <img src="/logo/logo_harmonize.png" alt="logo harmonize"/>   
         </div>
     )
 }
@@ -102,6 +212,7 @@ function FindTeachersDiv(){
 function SendCourseRequest({teacher, onClose}){
     useEffect(() => {
         document.querySelector(".searchTeacher").scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        
     }, []);
     const[request, setRequest] = useState({});
     const [user, setUser] = useState({});
